fc44d4b929cd2fabd204a2b075aa2719f5281f1a
ZippyMagic <zippymagician1@gmail.com>
2017-05-31 12:58:39 -0400

    Update and rename root.js to root.py

--- main/root.js
+++
@@ -1,9 +0,0 @@
-function parseResponse(e) {
-  var lang = "+-*/";
-  var I = 0;
-  if(e.charAt[1] === lang.charAt[I]) {
-    console.log(e.charAt[0] + lang.charAt[I] + e.charAt[2]);
-  } else {
-    I++
-  }
-}
