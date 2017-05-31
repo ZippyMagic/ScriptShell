a720bb4ff7704f3d0f84e14bdc807f33eab7bca8
ZippyMagic <zippymagician1@gmail.com>
2017-05-31 10:59:18 -0400

    Update root.js

--- main/root.js
+++ main/root.js
@@ -1,3 +1,9 @@
 function parseResponse(e) {
-  e.charAt[6]
+  var lang = "+-*/";
+  var I = 0;
+  if(e.charAt[1] === lang.charAt[I]) {
+    console.log(e.charAt[0] + lang.charAt[I] + e.charAt[2]);
+  } else {
+    I++
+  }
 }
