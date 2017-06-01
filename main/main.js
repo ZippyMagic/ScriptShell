1fa3cffee290fa6c8f3b66969442698ff8a6460d
ZippyMagic <zippymagician1@gmail.com>
2017-06-01 14:00:25 -0400

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,5 @@
 function runCode(e) {
-  document.getElementById('cOutput').value = eval(e);
+  eval(e);
+  var l = document.getElementById('cOutput').value;
+  document.getElementById('cOutput').value = l + "Command Ran! Did it work out the way you intended?";
 }
